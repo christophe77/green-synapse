a7e9b5fdb355194289601ce9ab2c98385ac6ff11
christophe77 <chrisb@gmx.fr>
2025-03-19 00:09:39 +0100

    allowedHosts

--- syn/vite.config.ts
+++ syn/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		allowedHosts: ['vps-32f8344e.vps.ovh.net']
+	}
 })
